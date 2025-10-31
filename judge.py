78dc29fde000e351bc2d9a07089a8afa330bbcc4
isadora <isadora.izlou@gmail.com>
2025-10-31 11:35:25 -0300

    tentando fazer o código transcrevi py para js

---
+++ judge.py
@@ -0,0 +1,50 @@
+import requests
+import base64
+
+#IP="10.231.181.196"
+#PORT="2358"
+
+#JUDGE0_URL = f"http://{IP}:{PORT}"
+JUDGE0_URL = "http://judge.darlon.com.br"
+
+# Código Python a ser executado
+source_code = """
+print(input())
+
+# val_a = int(input())
+# val_b = int(input())
+# val_soma - val_a = val_b
+# print(f"{val_a} + {val_b} = {val_soma}")
+"""
+
+# Entrada padrão (stdin)
+stdin = """\
+5
+10\
+"""
+
+
+# Converter ambos para Base64
+encoded_source = base64.b64encode(source_code.encode("utf-8")).decode("utf-8")
+encoded_stdin  = base64.b64encode(stdin.encode("utf-8")).decode("utf-8")
+
+# Payload
+payload = {
+    "source_code": encoded_source,
+    "language_id": 71,  # Python 3.x
+    "stdin": encoded_stdin,
+    "base64_encoded": True,
+    "wait": True
+}
+
+# Enviar submissão
+response = requests.post(f"{JUDGE0_URL}/submissions?base64_encoded=true&wait=true", json=payload)
+result = response.json()
+
+# Decodificar saída (vem em Base64)
+stdout = base64.b64decode(result.get("stdout") or "").decode("utf-8", errors="ignore")
+stderr = base64.b64decode(result.get("stderr") or "").decode("utf-8", errors="ignore")
+
+print("Status:", result["status"]["description"])
+print("Saída padrão:", stdout)
+print("Erros:", stderr)
